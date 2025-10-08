6f47b74d0a2758de25b8c829f1ca14887aafb0d3
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-08 22:54:52 +0700

    New table for user in sqflite

---
+++ lib/services/usersdb.dart
@@ -0,0 +1,47 @@
+import 'package:fukuro/models/user.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:fukuro/services/dbservice.dart';
+
+class UsersDb {
+  String TABLE = "users";
+  String ID = "id";
+  String NAME = "name";
+  String EMAIL = "email";
+  String PASSWORD = "password";
+
+  DBService dbService = DBService();
+
+
+  createTableUsers(db) async {
+    await db.execute('''
+      CREATE TABLE IF NOT EXISTS $TABLE (
+        $ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        $NAME TEXT NOT NULL,
+        $EMAIL TEXT NOT NULL UNIQUE,
+        $PASSWORD TEXT NOT NULL
+      )
+    ''');
+  }
+
+  void insert(Map<String, dynamic> data) async {
+    Database db = await dbService.getDatabase();
+    db.insert(TABLE, data);
+  }
+
+  Future <UserModel?> getOne(String email) async {
+    try {
+      Database db = await dbService.getDatabase();
+      var data = await db.query(TABLE, where: "$EMAIL = ?", whereArgs: [email]);
+      UserModel user = UserModel(
+        id: data.first['id'].toString(),
+        name: data.first['name'].toString(),
+        email: data.first['email'].toString(),
+        password: data.first['password'].toString(),
+      );
+
+      return user;
+    } catch (err) {
+      return null;
+    }   
+  }
+}
