0f3e5ecf7ef1552bb116e10a3163c229ca98f8b8
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-08 22:53:42 +0700

    Add login funct

--- lib/providers/profile.dart
+++ lib/providers/profile.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:fukuro/models/user.dart';
 import 'package:fukuro/services/sharedpref.dart';
+import 'package:fukuro/services/usersdb.dart';
 
 class Profile with ChangeNotifier {
+  UsersDb usersDb = UsersDb();
+
   bool isDark = sharedPref.getMode();
   bool isLoggedIn = sharedPref.getLoginStatus();
 
@@ -15,5 +19,14 @@ class Profile with ChangeNotifier {
     isLoggedIn = !isLoggedIn;
     notifyListeners();
     sharedPref.setLoginStatus(isLoggedIn);
+  }
+
+  Future <String> login(String email, String password) async {
+    UserModel? user = await usersDb.getOne(email);
+    if (user == null) return "User not found";
+    if (user.password != password) return "Incorrect password";
+
+    changeLoginStatus();
+    return "";
   }
 }
