e9bcbc2629f5127016c8445c70f0e022785fdf03
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-10 19:54:05 +0700

    Add new attribute [ prodile ] for UserModel

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,8 +1,9 @@
 class UserModel {
-  UserModel({required this.name, required this.id, required this.email, required this.password});
+  UserModel({required this.name, required this.id, required this.email, required this.password, required this.profile});
   
   String id;
   String name;
   String email;
   String password;
+  String profile;
 }
