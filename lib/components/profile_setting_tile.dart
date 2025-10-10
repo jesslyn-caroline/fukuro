4c3b53619dfdce2f253fa76f582216fe26b62b48
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-10 22:43:53 +0700

    Add subtitle and padding

--- lib/components/profile_setting_tile.dart
+++ lib/components/profile_setting_tile.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
 
 class ProfileSettingTile extends StatelessWidget {
-  ProfileSettingTile({super.key, required this.action, required this.title, required this.trailing});
+  ProfileSettingTile({super.key, required this.action, required this.title, this.subtitle, required this.trailing});
 
   void Function() action;
   String title;
+  String? subtitle;
   Widget trailing;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      contentPadding: EdgeInsets.fromLTRB(16, 6, 16, 6),
       tileColor: Theme.of(context).colorScheme.secondary.withOpacity(0.1),
       shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       onTap: action,
@@ -17,6 +19,7 @@ class ProfileSettingTile extends StatelessWidget {
         color: Theme.of(context).colorScheme.primary, 
         fontWeight: FontWeight.w800
       ),),
+      subtitle: subtitle != null ? Text("$subtitle", style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Theme.of(context).colorScheme.primary),) : null,
       trailing: trailing,
     );
   }
