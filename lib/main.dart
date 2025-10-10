77a93fbd8322e5c6168083705f5aa827e73ccf51
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-10 19:52:42 +0700

    Change Profile Provider name

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:sqflite_common_ffi/windows/sqflite_ffi_setup.dart';
 
-import 'package:fukuro/providers/profile.dart';
+import 'package:fukuro/providers/profile_provider.dart';
 import 'package:fukuro/screens/welcome.dart';
 import 'package:fukuro/services/sharedpref.dart';
 import 'package:fukuro/styles/darktheme.dart';
@@ -18,7 +18,7 @@ void main () {
 
   runApp(MultiProvider(
     providers: [
-      ChangeNotifierProvider(create: (context) => Profile()),
+      ChangeNotifierProvider(create: (context) => ProfileProvider()),
     ],
     child: MyApp(),
   ));
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      themeMode: context.watch<Profile>().isDark ? ThemeMode.dark : ThemeMode.light,
+      themeMode: context.watch<ProfileProvider>().isDark ? ThemeMode.dark : ThemeMode.light,
       debugShowCheckedModeBanner: false,
       theme: lightTheme,
       darkTheme: darkTheme,
