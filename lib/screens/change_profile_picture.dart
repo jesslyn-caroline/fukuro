1dfa22f85ae48c486b9506a9bd4f5bfeb01e7a5b
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-10 19:55:45 +0700

    New page

---
+++ lib/screens/change_profile_picture.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class ChangeProfilePicture extends StatefulWidget {
+  ChangeProfilePicture({super.key});
+
+  @override
+  State<ChangeProfilePicture> createState() => _ChangeProfilePictureState();
+}
+
+class _ChangeProfilePictureState extends State<ChangeProfilePicture> {
+  List <String> profilePic = ["pp-bear.png", "pp-rabbit.png", "pp-dino.png", "pp-racoon.png"];
+
+  String image = "pp-bear.png";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        actionsPadding: EdgeInsets.all(12),
+        actions: [
+          TextButton(onPressed: () {}, child: Text("Save", style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.white),))
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 160,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16))
+            ),
+            child: Column(
+              children: [
+                CircleAvatar(
+                  radius: 50,
+                  backgroundImage: Image.asset("assets/images/${image}").image,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+              child: GridView.builder(
+                padding: EdgeInsets.all(14),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), 
+                itemCount: profilePic.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: EdgeInsets.all(18),
+                    child: GestureDetector(
+                      onTap: () => setState(() {image = profilePic[index];}),
+                      child: CircleAvatar(
+                        radius: 30,
+                        backgroundImage: Image.asset("assets/images/${profilePic[index]}").image,
+                      ),
+                    ),
+                  );
+                },
+              )
+          ) 
+        ],
+      )
+    );
+  }
+}
---
+++ lib/screens/profile.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:fukuro/providers/profile_provider.dart';
+import 'package:fukuro/screens/change_profile_picture.dart';
+import 'package:provider/provider.dart';
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        children: [
+          Container(
+            width: double.infinity,
+            height: 220,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16))
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap:() => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChangeProfilePicture())),
+                  child: CircleAvatar(
+                    radius: 50,
+                    backgroundImage: Image.asset("assets/images/pp-bear.png").image,
+                  ),
+                ),
+                SizedBox(height: 10,),
+                Text("${context.watch<ProfileProvider>().name}", style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.white, fontSize: 18, fontWeight: FontWeight.w800),)
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(14),
+            child: Column(
+              children: [
+                ListTile(
+                  tileColor: Theme.of(context).colorScheme.secondary.withOpacity(0.1),
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => ChangeProfilePicture())),
+                  title: Text("Change Profile Picture", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary, 
+                    fontWeight: FontWeight.w800
+                  ),),
+                  trailing: Icon(Icons.arrow_forward_ios, color: Theme.of(context).colorScheme.primary,),
+                ),
+                ListTile(
+                  title: Text("Switch mode", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.primary, 
+                    fontWeight: FontWeight.w800
+                  ),),
+                  trailing: Switch(
+                    value: context.watch<ProfileProvider>().isDark, 
+                    onChanged: (value) => Provider.of<ProfileProvider>(context, listen: false).changeTheme(value),
+                  ),
+                )
+              ],
+            )
+          )
+        ],
+      )
+    );
+  }
+}
