33a600b353a98bf94d86ec6b802f352b8632cf3d
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-10 19:55:57 +0700

    Add bottom nav for pages

---
+++ lib/screens/index.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:fukuro/screens/home.dart';
+import 'package:fukuro/screens/profile.dart';
+
+class Index extends StatefulWidget {
+  const Index({super.key});
+
+  @override
+  State<Index> createState() => _IndexState();
+}
+
+class _IndexState extends State<Index> {
+
+  List <Map <String, dynamic>> screens = [
+    { "screen" : Home(), "icon" : Icons.home_filled, "label" : "Home" },
+    { "screen" : Profile(), "icon" : Icons.person_2, "label" : "Profile" },
+  ];
+
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: screens[index]["screen"],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: index,
+        onTap: (value) => setState(() {index = value;}),
+        items: [
+          ...screens.map((e) {
+            return BottomNavigationBarItem(icon: Icon(e["icon"], size: 30,), label: e["label"]);
+          })
+        ]
+      ),
+    );
+  }
+}
