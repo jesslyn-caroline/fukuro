fcab266988167ed96ccd935f9d6640bb092c4e3a
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-11 12:07:53 +0700

    Add function to update profile picture

--- lib/screens/change_profile_picture.dart
+++ lib/screens/change_profile_picture.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:fukuro/providers/profile_provider.dart';
 
 class ChangeProfilePicture extends StatefulWidget {
   ChangeProfilePicture({super.key});
@@ -9,8 +11,16 @@ class ChangeProfilePicture extends StatefulWidget {
 
 class _ChangeProfilePictureState extends State<ChangeProfilePicture> {
   List <String> profilePic = ["pp-bear.png", "pp-rabbit.png", "pp-dino.png", "pp-racoon.png"];
+  String? getProfilePic () { return context.read<ProfileProvider>().currentUser?.profile; }
 
-  String image = "pp-bear.png";
+  String? image;
+
+  @override
+  void initState() {
+    image = getProfilePic();
+    super.initState();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -20,7 +30,11 @@ class _ChangeProfilePictureState extends State<ChangeProfilePicture> {
         backgroundColor: Theme.of(context).colorScheme.primary,
         actionsPadding: EdgeInsets.all(12),
         actions: [
-          TextButton(onPressed: () {}, child: Text("Save", style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.white),))
+          TextButton(onPressed: () {  
+            Map <String, dynamic> data = { "email" : context.read<ProfileProvider>().currentUser?.email, "profile" : image };
+            context.read<ProfileProvider>().updateUserInfo(data);
+            Navigator.of(context).pop();
+          }, child: Text("Save", style: Theme.of(context).textTheme.bodyMedium!.copyWith(color: Colors.white),))
         ],
       ),
       body: Column(
@@ -30,35 +44,32 @@ class _ChangeProfilePictureState extends State<ChangeProfilePicture> {
             height: 160,
             decoration: BoxDecoration(
               color: Theme.of(context).colorScheme.primary,
-              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16))
-            ),
+              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(16), bottomRight: Radius.circular(16))),
             child: Column(
               children: [
                 CircleAvatar(
                   radius: 50,
-                  backgroundImage: Image.asset("assets/images/${image}").image,
-                ),
+                  backgroundImage: Image.asset("assets/images/${image}").image,),
               ],
             ),
           ),
           Expanded(
-              child: GridView.builder(
-                padding: EdgeInsets.all(14),
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), 
-                itemCount: profilePic.length,
-                itemBuilder: (context, index) {
-                  return Padding(
-                    padding: EdgeInsets.all(18),
-                    child: GestureDetector(
-                      onTap: () => setState(() {image = profilePic[index];}),
-                      child: CircleAvatar(
-                        radius: 30,
-                        backgroundImage: Image.asset("assets/images/${profilePic[index]}").image,
-                      ),
-                    ),
-                  );
-                },
-              )
+            child: GridView.builder(
+              padding: EdgeInsets.all(14),
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3), 
+              itemCount: profilePic.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: EdgeInsets.all(18),
+                  child: GestureDetector(
+                    onTap: () => setState(() {image = profilePic[index];}),
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: Image.asset("assets/images/${profilePic[index]}").image,),
+                  ),
+                );
+              },
+            )
           ) 
         ],
       )
