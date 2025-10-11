afb22db738e895743f2dae97c4f3ec90349380c3
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-11 14:33:05 +0700

    Update page

--- lib/screens/change_username.dart
+++ lib/screens/change_username.dart
@@ -1,10 +1,64 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:fukuro/components/blockbutton.dart';
+import 'package:fukuro/components/blockfield.dart';
+import 'package:fukuro/providers/profile_provider.dart';
 
 class ChangeUsername extends StatelessWidget {
-  const ChangeUsername({super.key});
+  ChangeUsername({super.key});
+
+  TextEditingController nameC = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 90,
+        leading: Padding(
+          padding: const EdgeInsets.all(14),
+          child: IconButton(
+            hoverColor: Colors.transparent,
+            splashColor: Colors.transparent,
+            onPressed: () => Navigator.of(context).pop(), 
+            icon: Icon(Icons.arrow_back, size: 24)
+          ),
+        ),
+      ),
+      body: Stack(
+        alignment: Alignment.bottomRight,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20, vertical: 24),
+            child: ListView(
+              children: [
+                Text(
+                  "Change username",
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w900,
+                    color: Theme.of(context).colorScheme.primary
+                  ),
+                ),               
+                SizedBox(height: 20),
+                BlockField(hintText: "New username", controller: nameC, errorText: "", isPassword: false),
+                SizedBox(height: 28),
+                BlockButton(
+                  text: "CHANGE",
+                  action: () {
+                    Map <String, dynamic> data = { "email" : context.read<ProfileProvider>().currentUser?.email, "name" : nameC.text};
+                    context.read<ProfileProvider>().updateUserInfo(data);
+                    Navigator.of(context).pop();
+                  },
+                  bgColor: Theme.of(context).colorScheme.primary,
+                  textColor: Colors.white,
+                  borderColor: Theme.of(context).colorScheme.primary
+                )
+              ],
+            ),
+          ),
+          Image.asset("assets/images/peekaboo-owl.png", width: 150, fit: BoxFit.cover),
+        ]
+      ),
+    );
   }
 }
