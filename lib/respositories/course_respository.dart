4fefd17fbfbdb25fb7e025670a61cf079584bcb6
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-23 10:40:00 +0700

    New Respository to fetch course data

---
+++ lib/respositories/course_respository.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+
+import 'package:fukuro/models/course_model.dart';
+import 'package:http/http.dart' as http;
+
+class CourseRespository {
+  // CourseRespository();
+  String URL = "https://fukuro-api.vercel.app/courses";
+
+  Future <List<CourseModel>> fetch() async {
+    try {
+      var response = await http.get(Uri.parse(URL));
+      var result = jsonDecode(response.body);
+
+      List <CourseModel> courses = [ ...result["data"].map((e) => CourseModel.fromJson(e)) ];
+
+      if (response.statusCode == 200) {
+        return courses;
+      }
+
+    } catch (err) {
+      print(err);
+    }
+
+    return [];
+  }
+}
