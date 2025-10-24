6d7bd2ca33a781547bec523e05d016da91d7d779
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-10-24 23:11:41 +0700

    Add navigation to daily quiz

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:fukuro/components/course_card.dart';
+import 'package:fukuro/components/course_card_1.dart';
 import 'package:fukuro/components/progress_card.dart';
 import 'package:fukuro/providers/profile_provider.dart';
+import 'package:fukuro/screens/daily_quiz.dart';
 import 'package:provider/provider.dart';
 
 class Home extends StatelessWidget {
@@ -76,21 +78,64 @@ class Home extends StatelessWidget {
                 Row(
                   children: [
                     ProgressCard(title: "Ongoing", num: 3, img: "assets/images/bulb.png"),
-                    SizedBox(width: 8),
+                    SizedBox(width: 12),
                     ProgressCard(title: "Completed", num: 1, img: "assets/images/goal.png"),
                   ],
                 ),
+                SizedBox(height: 12),
+                GestureDetector(
+                  onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => DailyQuiz())),
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(18, 16, 18, 10),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      border: Border.all(color: Theme.of(context).colorScheme.primary)
+                    ),
+                    child: Row(
+                      children: [
+                        Image.asset("assets/images/quiz.png", width: 50),
+                        SizedBox(width: 16),
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Ready for Today's challenge?", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w900,
+                                  color: Theme.of(context).colorScheme.primary
+                                ),
+                              ),
+                              SizedBox(height: 2),
+                              Text(
+                                "Test your knowledge with fun, bite-sized quizzes!", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                                  fontSize: 12,
+                                  fontWeight: FontWeight.w800,
+                                  color: Theme.of(context).colorScheme.secondary
+                                ),
+                                softWrap: true,
+                              ),
+                              SizedBox(height: 4),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),           
                 SizedBox(height: 20),
                 Container(
+                  padding: EdgeInsets.fromLTRB(16, 12, 0, 0),
                   decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(10),
                     color: Theme.of(context).colorScheme.secondary
                   ),
                   child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       Expanded(
                         child: Padding(
-                          padding: EdgeInsets.only(left: 20, top: 12, bottom: 12),
+                          padding: EdgeInsets.only(bottom: 12),
                           child: Text(
                             "What would you like to learn today?",
                             style: Theme.of(context).textTheme.bodyMedium!.copyWith(
@@ -101,7 +146,7 @@ class Home extends StatelessWidget {
                           ),
                         ),
                       ),
-                      Image.asset("assets/images/illustration.png", width: 140)
+                      Image.asset("assets/images/illustration.png", width: 150)
                     ],
                   ),
                 ),
@@ -136,21 +181,9 @@ class Home extends StatelessWidget {
                 SizedBox(height: 12),
                 Column(
                   children: [
-                    CourseCard(
-                      title: "React JS for Front-End Web Dev",
-                      subtitle: "Intermediate: 40 lessons",
-                      imgSrc: "assets/images/reactjs-fill.png",
-                    ),
-                    CourseCard(
-                      title: "Python 101: The Basics",
-                      subtitle: "Beginner: 20 lessons",
-                      imgSrc: "assets/images/python.png",
-                    ),
-                    CourseCard(
-                      title: "Your First Web: HTML and CSS",
-                      subtitle: "Beginner: 25 lessons",
-                      imgSrc: "assets/images/web.png",
-                    )
+                    CourseCard1(id: "68f5ad59a870cf5c0c8a7f6a", title: "Python for Starters", numOfLessons: 12, level: "Beginner"),
+                    CourseCard1(id: "68f5ae0b89b6d338708a6627", title: "Web Basics with HTML and CSS", numOfLessons: 25, level: "Beginner"),
+                    CourseCard1(id: "68f5ae6d89b6d338708a662a", title: "Get to Know JavaScript", numOfLessons: 20, level: "Beginner"),
                   ],
                 )
               ],
