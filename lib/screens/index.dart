e0c87e900145db7ecd1489bf9700fef69fc1d30b
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-11-13 00:14:18 +0700

    Comment sqflite_ffi

--- lib/screens/index.dart
+++ lib/screens/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:fukuro/screens/courses.dart';
 import 'package:fukuro/screens/home.dart';
 import 'package:fukuro/screens/profile.dart';
@@ -11,12 +12,17 @@ class Index extends StatefulWidget {
 }
 
 class _IndexState extends State<Index> {
+  FirebaseAnalytics analytics = FirebaseAnalytics.instance;
 
   List <Map <String, dynamic>> screens = [
     { "screen" : Home(), "icon" : Icons.home_rounded, "label" : "Home" },
     { "screen" : Courses(), "icon" : Icons.book_rounded, "label" : "Courses" },
     { "screen" : Profile(), "icon" : Icons.person_2_rounded, "label" : "Profile" },
   ];
+
+  Future <void> logPage(String u, String v) async {
+    await analytics.logEvent(name: "${u}_to_${v}");
+  }
 
   int index = 0;
 
@@ -27,7 +33,12 @@ class _IndexState extends State<Index> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: index,
         selectedItemColor: Theme.of(context).colorScheme.primary,
-        onTap: (value) => setState(() => index = value),
+        onTap: (value) {
+          logPage(screens[index]["label"], screens[value]["label"]);
+          index = value;
+          
+          setState(() {});
+        },
         items: [
           ...screens.map((e) {
             return BottomNavigationBarItem(icon: Icon(e["icon"]), label: e["label"]);
