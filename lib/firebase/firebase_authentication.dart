7fe3e25b7c2c4096faaf2876797fab7aea8b8aed
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-12 16:13:40 +0700

    Adding features to update profile

--- lib/firebase/firebase_authentication.dart
+++ lib/firebase/firebase_authentication.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 class FirebaseAuthenticationService {
 
-  Future <String> signup(String email, String password) async {
+  Future <String> signup(String email, String password, String name) async {
     String msg = "";
 
     try {
@@ -11,6 +11,11 @@ class FirebaseAuthenticationService {
         password: password
       );
       msg = "";
+
+      await FirebaseAuth.instance.currentUser?.updateProfile(
+        displayName: name,
+        photoURL: 'pp-dino.png'
+      );
 
     } on FirebaseAuthException catch (err) {
       if (err.code == "email-already-in-use") msg = "Email already in use";
@@ -21,10 +26,35 @@ class FirebaseAuthenticationService {
     return msg;
   }
 
-  Future <void> changePassword (String password) async {
+  Future <void> changePassword (String oldPassword, String password) async {
     try {
       var user = FirebaseAuth.instance.currentUser;
-      user!.updatePassword(password);
+      final cred = EmailAuthProvider.credential(
+        email: user!.email!, 
+        password: oldPassword
+      );
+      await user.reauthenticateWithCredential(cred);
+      await user.updatePassword(password);
+
+      print("CHANGED $user");
+    } on FirebaseAuthException catch (err) {
+      // if (err.code == ) 
+    }
+  }
+  
+  Future <void> changeProfilePic (String pic) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+      await user!.updatePhotoURL(pic);
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  Future <void> changeDisplayName (String name) async {
+    try {
+      var user = FirebaseAuth.instance.currentUser;
+      await user!.updateDisplayName(name);
     } catch (err) {
       print(err);
     }
@@ -50,7 +80,5 @@ class FirebaseAuthenticationService {
     return msg;
   }
 
-  Future <void> logout() async {
-    await FirebaseAuth.instance.signOut();
-  }
+  Future <void> logout() async => await FirebaseAuth.instance.signOut();
 }
