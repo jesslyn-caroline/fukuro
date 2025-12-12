4e8cbb76b5bd1ef219264de557050d67d086390e
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-12 16:13:09 +0700

    Change Button Color into primary color

--- lib/components/blockbutton.dart
+++ lib/components/blockbutton.dart
@@ -5,14 +5,16 @@ class BlockButton extends StatelessWidget {
     super.key,
     required this.text,
     required this.action,
-    required this.bgColor,
-    required this.textColor,
-    required this.borderColor
+    this.bgColor,
+    this.textColor,
+    this.borderColor,
+    this.isDisabled = false,
   });
   
   String text;
   void Function() action;
-  Color bgColor, textColor, borderColor;
+  Color? bgColor, textColor, borderColor;
+  bool isDisabled;
 
   @override
   Widget build(BuildContext context) {
@@ -22,19 +24,19 @@ class BlockButton extends StatelessWidget {
       height: 44,
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
-          backgroundColor: bgColor,
+          backgroundColor: bgColor == null? Theme.of(context).colorScheme.primary : bgColor,
           shape: RoundedRectangleBorder(
             borderRadius: BorderRadius.circular(10)
           ),
-          side: BorderSide(color: borderColor, width: 1.5)
+          side: BorderSide(color: Theme.of(context).colorScheme.primary, width: 1.5)
         ),
-        onPressed: action, 
+        onPressed: isDisabled? null : action, 
         child: Text(
           text,
           style: Theme.of(context).textTheme.bodyMedium!.copyWith(
             fontSize: 16,
             fontWeight: FontWeight.w700,
-            color: textColor
+            color: textColor == null? Colors.white : textColor
           )
         )
       ),
