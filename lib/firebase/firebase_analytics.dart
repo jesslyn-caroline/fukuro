6cf43bd6d9a0f2d2e8d139ed571b14978bc17872
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-12 10:11:36 +0700

    Change firebase analytics params

--- lib/firebase/firebase_analytics.dart
+++ lib/firebase/firebase_analytics.dart
@@ -7,13 +7,27 @@ class FirebaseAnalyticsServices {
     await analytics.logEvent(name: "${u}_to_${v}");
   }
 
-  Future <void> logCourse(String title) async {
-    await analytics.logEvent(name: "open_course_${title.replaceAll(' ', '_')}");
+  Future <void> logCourse(String id, String courseName, String level) async {
+    await analytics.logEvent(
+      name: 'enroll_button_clicked',
+      parameters: {
+        'course_id': id,
+        'course_name': courseName,
+        'level': level,
+        'time': DateTime.now().toIso8601String(),
+      },
+
+    );
   }
 
-  Future <void> logQuiz() async {
-    DateTime curr = DateTime.now();
-    String time = "${curr.day}_${curr.month}_${curr.year}_${curr.hour}_${curr.minute}_${curr.second}";
-    await analytics.logEvent(name: "play_quiz_on_${time}");
+  Future <void> toQuiz() async {
+    await analytics.logEvent(name: "to_quiz");
+  }
+
+  Future <void> logQuiz(int score) async {
+    await analytics.logEvent(
+      name: 'quiz_button_clicked',
+      parameters: {'action': 'quiz button clicked', 'value': score.toString()},
+    );
   }
 }
