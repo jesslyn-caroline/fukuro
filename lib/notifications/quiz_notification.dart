f8758f86a9877532e06a664eeb542dda55da1ada
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-26 01:01:04 +0700

    Update

--- lib/notifications/quiz_notification.dart
+++ lib/notifications/quiz_notification.dart
@@ -1,3 +1,17 @@
+import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:fukuro/notifications/notification.dart';
+
 class QuizNotification {
-  
+  NotificationInit _notificationInit = NotificationInit();
+
+  Future <void> showNotification() async {
+    AwesomeNotifications().createNotification(
+      content: NotificationContent(
+        id: 1, 
+        channelKey: _notificationInit.quizNotifChannelKey,
+        title: 'See you tomorrow!',
+        body: 'Don\'t forget to your quiz tomorrow to extend your streak.'
+      )
+    );
+  }
 }
