36fc38143e8d7f759337e03e1fc124a4071439fc
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-26 19:15:29 +0700

    Update content

--- lib/screens/reminder.dart
+++ lib/screens/reminder.dart
@@ -18,38 +18,56 @@ class _ReminderState extends State<Reminder> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: EdgeInsets.fromLTRB(24, 20, 24, 20),
+        padding: EdgeInsets.fromLTRB(24, 90, 24, 20),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            BannerCard( text: "We will remind you at scheduled time!", semanticsLabel: "Reminder Banner", image: "assets/images/clock.png",),
-            SizedBox(height: 16,),
-            Center(
-              child: Column(
-                children: [
-                  Image.asset("assets/images/${_reminderService.selectedTime == null ? "own-on-moon" : "owl" }.png"),
-                  Text( _reminderService.selectedTime == null ? 
-                    "No study sessions scheduled yet." :
-                    "You have a study session scheduled at ${DateFormat.yMMMMd().format(_reminderService.selectedTime!)} ${DateFormat.Hm().format(_reminderService.selectedTime!)}.",
-                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w700,
-                      color: Theme.of(context).colorScheme.primary
-                    ),
-                    textAlign: TextAlign.center
-                  ),
-                  Button(
-                    action: () async {
-                      await _reminderService.showDatePicker(context);
-                      await _reminderService.showTimePicker(context);
-                      await _reminderService.schedule();
-                      setState(() {});
-                    }, 
-                    text: "Schedule now!",
-                  )
-                ],
-              ),
+            Image.asset("assets/images/alarm-clock.png", width: 200),
+            ListTile(
+              title: Text("Date", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+                color: Theme.of(context).colorScheme.primary
+              )),
+              subtitle: Text("${_reminderService.selectedTime != null ? DateFormat.yMMMMd().format(_reminderService.selectedTime!) : "-"}", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                fontSize: 25,
+                fontWeight: FontWeight.w900,
+                color: Theme.of(context).colorScheme.primary
+              )),
+              onTap: () async {
+                await _reminderService.showDatePicker(context);
+                setState(() {});
+              },
+            ),
+            ListTile(
+              contentPadding: EdgeInsets.zero,
+              leading: Icon(Icons.timer_sharp),
+              title: Text("Time", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                fontSize: 20,
+                fontWeight: FontWeight.w900,
+                color: Theme.of(context).colorScheme.primary
+              )),
+              subtitle: Text("${_reminderService.selectedTime != null ? DateFormat.Hm().format(_reminderService.selectedTime!) : "-"}", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                fontSize: 25,
+                fontWeight: FontWeight.w900,
+                color: Theme.of(context).colorScheme.primary
+              )),
+              onTap: () async {
+                await _reminderService.showTimePicker(context);
+                setState(() {});
+              },
             )
+            // Text("Hi, I'm Fuko!", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+            //   fontSize: 20,
+            //   fontWeight: FontWeight.w900,
+            //   color: Theme.of(context).colorScheme.primary
+            // )),
+            // Text("Your study reminder", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+            //   fontSize: 20, 
+            //   fontWeight: FontWeight.w900, 
+            //   color: Theme.of(context).colorScheme.secondary
+            // )),
           ],
         ),
       ),
