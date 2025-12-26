07932717e79714a1a3d5150d753cb6b28e046f2e
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-26 12:36:53 +0700

    Update content

--- lib/screens/reminder.dart
+++ lib/screens/reminder.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:fukuro/components/button.dart';
 import 'package:fukuro/components/cards/banner_card.dart';
+import 'package:fukuro/services/reminder_service.dart';
+import 'package:intl/intl.dart';
 
 class Reminder extends StatefulWidget {
   const Reminder({super.key});
@@ -9,15 +12,44 @@ class Reminder extends StatefulWidget {
 }
 
 class _ReminderState extends State<Reminder> {
+  ReminderService _reminderService = ReminderService();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
         padding: EdgeInsets.fromLTRB(24, 20, 24, 20),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             BannerCard( text: "We will remind you at scheduled time!", semanticsLabel: "Reminder Banner", image: "assets/images/clock.png",),
-            
+            SizedBox(height: 16,),
+            Center(
+              child: Column(
+                children: [
+                  Image.asset("assets/images/${_reminderService.selectedTime == null ? "own-on-moon" : "owl" }.png"),
+                  Text( _reminderService.selectedTime == null ? 
+                    "No study sessions scheduled yet." :
+                    "You have a study session scheduled at ${DateFormat.yMMMMd().format(_reminderService.selectedTime!)} ${DateFormat.Hm().format(_reminderService.selectedTime!)}.",
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      fontSize: 16,
+                      fontWeight: FontWeight.w700,
+                      color: Theme.of(context).colorScheme.primary
+                    ),
+                    textAlign: TextAlign.center
+                  ),
+                  Button(
+                    action: () async {
+                      await _reminderService.showDatePicker(context);
+                      await _reminderService.showTimePicker(context);
+                      await _reminderService.schedule();
+                      setState(() {});
+                    }, 
+                    text: "Schedule now!",
+                  )
+                ],
+              ),
+            )
           ],
         ),
       ),
