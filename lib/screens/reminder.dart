af63cb559abee02c0810abbca5aec4bb2c9340d7
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-26 22:02:54 +0700

    Update content

--- lib/screens/reminder.dart
+++ lib/screens/reminder.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:fukuro/components/blockbutton.dart';
 import 'package:fukuro/components/reminder_tile.dart';
+import 'package:fukuro/providers/profile_provider.dart';
+import 'package:fukuro/screens/profile.dart';
 import 'package:fukuro/services/reminder_service.dart';
 import 'package:intl/intl.dart';
+import 'package:provider/provider.dart';
 
 class Reminder extends StatefulWidget {
   const Reminder({super.key});
@@ -15,6 +18,13 @@ class _ReminderState extends State<Reminder> {
   ReminderService _reminderService = ReminderService();
 
   @override
+  void initState() {
+    // TODO: implement initState
+    _reminderService.initSelectedTime(context);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
@@ -23,9 +33,9 @@ class _ReminderState extends State<Reminder> {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Image.asset("assets/images/alarm-clock.png", width: 200),
+            Image.asset(_reminderService.isSet ? "assets/images/alarm-clock-smile.png" : "assets/images/alarm-clock.png", width: 200),
             SizedBox(height: 16),
-            Text("Schedule your study session!", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+            Text(_reminderService.isSet ? "We will remind you by the time!" : "Schedule your study session!", style: Theme.of(context).textTheme.bodyMedium!.copyWith(
               fontSize: 20,
               fontWeight: FontWeight.w900,
               color: Theme.of(context).colorScheme.primary
@@ -33,7 +43,9 @@ class _ReminderState extends State<Reminder> {
             SizedBox(height: 16),
             ReminderTile(
               title: "Date", 
-              subtitle: _reminderService.selectedTime != null ? DateFormat.yMMMMd().format(_reminderService.selectedTime!) : "Not set", 
+              subtitle: _reminderService.isSet ?
+                "${DateFormat.yMMMMd().format(context.read<ProfileProvider>().reminderTime!)}" : 
+                "${DateFormat.yMMMMd().format(_reminderService.selectedTime)}", 
               icon: Icons.calendar_month_outlined, 
               action: () async {
                 await _reminderService.showDatePicker(context);
@@ -42,7 +54,9 @@ class _ReminderState extends State<Reminder> {
             ),
             ReminderTile(
               title: "Time", 
-              subtitle: _reminderService.selectedTime != null ? DateFormat.Hm().format(_reminderService.selectedTime!) : "Not set", 
+              subtitle: _reminderService.isSet ?
+                "${DateFormat.Hm().format(context.read<ProfileProvider>().reminderTime!)}" : 
+                "${DateFormat.Hm().format(_reminderService.selectedTime)}", 
               icon: Icons.timer_outlined, 
               action: () async {
                 await _reminderService.showTimePicker(context);
@@ -50,7 +64,18 @@ class _ReminderState extends State<Reminder> {
               }
             ),
             SizedBox(height: 12),
-            BlockButton(text: "SET REMINDER", action: () {})
+            BlockButton(
+              text: _reminderService.isSet ? "SET REMINDER" : "CANCEL", 
+              action: () {
+                if (_reminderService.isSet) {
+                  context.read<ProfileProvider>().changeReminderTime(null);
+                  _reminderService.remove();
+                  return;
+                }
+                context.read<ProfileProvider>().changeReminderTime(_reminderService.selectedTime);
+                _reminderService.set();
+              }
+            )
           ],
         ),
       ),
