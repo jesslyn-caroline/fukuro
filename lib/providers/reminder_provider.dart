90928f9a07b2cc4e12df944b8cf49e3c24d5d751
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-27 21:52:45 +0700

    New provider for reminder

---
+++ lib/providers/reminder_provider.dart
@@ -0,0 +1,84 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker_plus/flutter_datetime_picker_plus.dart' as picker;
+import 'package:fukuro/notifications/reminder_notification.dart';
+import 'package:fukuro/services/sharedpref.dart';
+
+class ReminderProvider with ChangeNotifier {
+  ReminderNotification _reminderNotification = ReminderNotification();
+
+  DateTime? reminderTime = sharedPref.getReminderTime() != null? DateTime.parse(sharedPref.getReminderTime()!) : null;
+  DateTime? selectedTime = DateTime.now().add(Duration(minutes: 5));
+  Timer? timerValue;
+
+
+  Future <void> showDatePicker(context) async {
+    await picker.DatePicker.showDatePicker(context, 
+      minTime: DateTime.now(),
+      currentTime: DateTime.now(),
+      onConfirm: (time) => selectedTime = time,
+      theme: picker.DatePickerTheme(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        doneStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+          color: Theme.of(context).colorScheme.primary,
+          fontWeight: FontWeight.w900
+        ),
+        cancelStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+          color: Theme.of(context).colorScheme.secondary
+        ),
+        itemStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+          color: Theme.of(context).colorScheme.primary
+        ),
+        containerHeight: MediaQuery.of(context).size.height * 0.4,
+      ),
+    );
+  }
+
+  Future <void> showTimePicker(context) async {
+    await picker.DatePicker.showTimePicker(context, 
+      showSecondsColumn: false,
+      currentTime: DateTime.now(),
+      onChanged: (time) => selectedTime = time,
+      theme: picker.DatePickerTheme(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        doneStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+          color: Theme.of(context).colorScheme.primary,
+          fontWeight: FontWeight.w900
+        ),
+        cancelStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+          color: Theme.of(context).colorScheme.secondary
+        ),
+        itemStyle: Theme.of(context).textTheme.bodyMedium!.copyWith(
+          color: Theme.of(context).colorScheme.primary
+        ),
+        containerHeight: MediaQuery.of(context).size.height * 0.4,
+      ),
+    );
+  }
+
+  Future<void> set(BuildContext context) async {
+    reminderTime = selectedTime;
+    notifyListeners();
+    await _reminderNotification.showNotification(selectedTime!);
+
+    timerValue = Timer.periodic(Duration(seconds: 1), (timer) {
+      if (reminderTime!.isBefore(DateTime.now())) {
+        timerValue!.cancel();
+        sharedPref.delReminderTime();
+        reminderTime = null;
+        selectedTime = DateTime.now().add(Duration(minutes: 5));
+        notifyListeners(); 
+      } 
+    },);
+  }
+  
+  Future<void> remove() async {
+    await sharedPref.delReminderTime();
+    await _reminderNotification.cancelNotification();
+    timerValue!.cancel();
+    reminderTime = null;
+    notifyListeners();
+  }
+  
+}
