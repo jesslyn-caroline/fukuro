df0fc526511af5a69fab0d9229115bdae65ff927
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2025-12-27 15:14:45 +0700

    New component

---
+++ lib/components/ad_button.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+import 'package:fukuro/admob/rewarded_ad_service.dart';
+
+class AdButton extends StatelessWidget {
+  AdButton({super.key, required this.count});
+
+  RewardedAdService _rewardedAdService = RewardedAdService();
+  int count;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Theme.of(context).colorScheme.primary,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        padding: EdgeInsets.symmetric(horizontal: 12) 
+      ),
+      child: Row(
+        spacing: 5,
+        children: [
+          Text(count.toString(), style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+            color: Colors.white, 
+            fontSize: 16,
+            fontWeight: FontWeight.w900
+          )),
+          Image.asset("assets/images/key.png", width: 18,)
+        ],
+      ),
+      onPressed: () async {
+        // await _rewardedAdService.showAd();
+        _rewardedAdService.loadAd();
+      },
+    );
+  }
+}
