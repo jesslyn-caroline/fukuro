320efc20c8022ce3e1dc761d0be358dcf9963af8
jesslyn-caroline <jesslyn.caroline2005@gmail.com>
2026-01-20 13:49:51 +0700

    Removed unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -3,12 +3,10 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:fukuro/notifications/notification.dart';
-import 'package:fukuro/notifications/quiz_notification.dart';
 import 'package:fukuro/providers/reminder_provider.dart';
 // import 'package:fukuro/screens/testing.dart';
 import 'package:provider/provider.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:awesome_notifications/awesome_notifications.dart';
 // import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 // import 'package:sqflite_common_ffi/windows/sqflite_ffi_setup.dart';
 
@@ -34,7 +32,7 @@ void main () async {
   runApp(MultiProvider(
     providers: [
       ChangeNotifierProvider(create: (context) => ProfileProvider()),
-      ChangeNotifierProvider(create: (context) => ReminderProvider(),)
+      ChangeNotifierProvider(create: (context) => ReminderProvider(),),
     ],
     child: MyApp(),
   ));
